901ce1ed1c2ee4721cc60400db087f5a269e54bd
Justin Cheng <jucheng.ca@gmail.com>
2023-01-12 19:19:49 -0800

    Complete bare_minimum/callbackReview.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,12 +6,27 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, 'utf8', (err, fileData) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var firstLine = fileData.split('\n').shift();
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
+  request(url, function(error, response, body) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
   // TODO
 };
 
